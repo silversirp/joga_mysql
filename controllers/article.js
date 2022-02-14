3dd79aa4615874263595868887c85537ec599bae
Silver Sirp <silver.sirp@khk.ee>
2022-02-15 01:58:51 +0200

    Created article controller and route, refactored application code

---
+++ controllers/article.js
@@ -0,0 +1,36 @@
+// import database connection
+const con = require('../utils/db')
+
+// show all articles index page
+const getAllArticles = (req, res) => {
+    let query = "SELECT * FROM article"
+    let articles = []
+    con.query(query, (err, result) => {
+        if (err) throw err
+        articles = result
+        res.render('index', {
+            articles: articles
+        })
+    })
+}
+
+// show article by this slug
+const getArticleBySlug = (req, res) => {
+    let query = `select article.id, article.name, article.slug, article.image, article.body, article.published, author.name as author, author.id as author_id from article JOIN author ON article.author_id = author.id where slug = "${req.params.slug}";`
+    // let query = `SELECT * FROM article where slug = "${req.params.slug}"`
+    let article
+    con.query(query, (err, result) => {
+        if (err) throw err
+        article = result
+        // console.log(result)
+        res.render('article', {
+            article: article
+        })
+    })
+}
+
+// export controller functions
+module.exports = {
+    getAllArticles,
+    getArticleBySlug
+}
---
+++ utils/db.js
@@ -0,0 +1,10 @@
+// create database connection
+const mysql = require('mysql')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'qwerty',
+    database: 'joga_mysql'
+})
+
+module.exports = db
