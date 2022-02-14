c70bdf77f83d11b91c06b35d5c544bd2e30918d1
Silver Sirp <silver.sirp@khk.ee>
2022-02-14 22:37:49 +0200

    Added author name into single article

--- index.js
+++ index.js
@@ -49,11 +49,13 @@ app.get('/', (req, res) => {
 
 // show article by this slug
 app.get('/article/:slug', (req, res) => {
-    let query = `SELECT * FROM article where slug = "${req.params.slug}"`
+    let query = `select article.id, article.name, article.slug, article.image, article.body, article.published, author.name as author from article JOIN author ON article.author_id = author.id where slug = "${req.params.slug}";`
+    // let query = `SELECT * FROM article where slug = "${req.params.slug}"`
     let article
     con.query(query, (err, result) => {
         if (err) throw err
         article = result
+        // console.log(result)
         res.render('article', {
             article: article
         })
